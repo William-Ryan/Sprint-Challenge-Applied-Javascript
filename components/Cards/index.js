ea4533391035774faf689298b4aea8c223d5bac0
William Ryan <billy.ryan.rom@gmail.com>
2020-01-17 13:33:44 -0500

    Add Object Loop to API

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,43 +17,43 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
-
 axios.get("https://lambda-times-backend.herokuapp.com/articles")
-.then(res => {
-    console.log(res);
-    const cardContainer = document.querySelector(".cards-container");
-    res.data.articles.bootstrap.forEach(el => {
-        const newCard = createCard(el);
-        cardContainer.append(newCard);
-    });
-})
-.catch(err => {
-    console.log("This is an Error",err);
-})
+    .then(response => {
+        console.log(response);
+        Object.values(response.data.articles).forEach(item => {
+            item.forEach(item => {
+                let cards = document.querySelector('.cards-container');
+                cards.append(newCard(item));
+            })
+        })
+    }) 
+    .catch(error => {
+        console.log(error);
+    })
 
-function createCard(obj){
-    
-    const card = document.createElement("div");
-    const headline = document.createElement("div");
-    const author = document.createElement("div");
-    const imgContainer = document.createElement("div");
-    const img = document.createElement("img");
-    const name = document.createElement("span");
+function newCard (obj){
+
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
 
     card.append(headline);
     card.append(author);
+    card.append(span);
     author.append(imgContainer);
     imgContainer.append(img);
-    author.append(name);
-
-    card.classList.add("card");
-    headline.classList.add("headline");
-    author.classList.add("author");
-    imgContainer.classList.add("img-container");
 
     headline.textContent = obj.headline;
     img.src = obj.authorPhoto;
-    author.textContent = obj.authorName;
+    span.textContent = `Author: ${obj.authorName}`
 
-    return card;
+    return card
 }
